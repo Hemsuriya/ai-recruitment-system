12129a101116784d538c6cd2348aa679beac9eca
Sudha2522 <Sudha.Deshpande@cbcinc.ai>
2026-03-17 20:40:37 +0530

    #added Hr layout [17/03/2026]

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./features/auth/LoginPage"
 import TemplateList from "../hr/Templates/TemplateList"
+import HrLayout from "./components/layouts/HrLayout"
 
 function App() {
   return (
    <BrowserRouter>
    <Routes>
+    {/* Login */}
     <Route path="/login" element={<LoginPage/>}/>
-    <Route path="/hr/templates" element={<TemplateList/>}/>
+
+    {/* HR Layouts routes */}
+     <Route path="/hr" element={<HrLayout />}></Route>
+     <Route path="/hr/templates" element={<TemplateList/>}/>
    </Routes>
    </BrowserRouter>
   );
